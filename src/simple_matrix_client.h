e0b2791eb4c8dbe54d08358fbb2033646a2f9406
jame25 <jame25@users.noreply.github.com>
2025-06-05 06:36:42 +0100

    Added: support for action message

--- src/simple_matrix_client.h
+++ src/simple_matrix_client.h
@@ -16,12 +16,13 @@ public:
     ~simple_matrix_client();
     
     void init(const std::string& homeserver, const std::string& token, const std::string& room_id);
-    bool send_message(const std::string& message);
+    bool send_message(const std::string& message, bool as_emote = false);
+    bool send_emote(const std::string& action);
     bool test_connection();
     std::string get_room_id() const { return m_room_id; }
     
 private:
-    std::string create_json_message(const std::string& message);
+    std::string create_json_message(const std::string& message, bool as_emote = false);
     bool send_http_request(const std::string& url, const std::string& method, const std::string& data, const std::string& headers);
     std::string url_encode(const std::string& value);
     std::string generate_txn_id();
